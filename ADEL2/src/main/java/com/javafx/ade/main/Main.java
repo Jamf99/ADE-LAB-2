6c794f78ea85e64cfa2787cc1411eacca291ba56
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2018-09-18 18:30:48 -0500

    MUSIC

--- ADEL2/src/main/java/com/javafx/ade/main/Main.java
+++ ADEL2/src/main/java/com/javafx/ade/main/Main.java
@@ -1,14 +1,20 @@
 package com.javafx.ade.main;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 
 public class Main extends Application {
 
 	private Stage window;
+	public static boolean reproductor;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -22,8 +28,24 @@ public class Main extends Application {
 		window.show();
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public static void main(String[] args) throws Exception {
+
+		if(reproductor == false){			
+			musica();
+			launch(args);
+		}
 	}
+    public static void musica()  throws Exception        
+    {
+        
+        String sonido = "./data/sonido/Fortnite wav.wav";
+        
+        InputStream in = new FileInputStream(sonido);
+        
+        AudioStream audio = new AudioStream(in);
+       
+        AudioPlayer.player.start(audio);
+    }
+
 	
 }
